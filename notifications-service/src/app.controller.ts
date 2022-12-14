61263e7a228ce8d327f800f873cefeaf84a73240
Paulo Pires <prpires66@gmail.com>
2022-12-14 18:54:05 -0300

    Aula 01 - 1:25

--- notifications-service/src/app.controller.ts
+++ notifications-service/src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
+import { from } from 'rxjs';
+import { PrismaService } from './prisma.service';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  list() {
+    return this.prisma.notification.findMany()
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Você tem uma nova solicitação de amizade!',
+        category: 'social',
+        recipientId: randomUUID(),
+      }
+    })
   }
 }

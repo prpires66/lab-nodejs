e5b8f08280416cfd2f41af7c55190d5ecb69dec3
Paulo Pires <prpires66@gmail.com>
2022-12-18 00:52:16 -0300

    Aula 03 - 18:29

--- notifications-service/src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ notifications-service/src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,19 +1,17 @@
-import { Injectable } from "@nestjs/common";
-import { Notification } from "src/application/entities/notification";
-import { NotificationsRepository } from "../../../../application/repositories/notifications-repository";
-import { PrismaService } from "../prisma.service";
+import { Injectable } from '@nestjs/common';
+import { Notification } from 'src/application/entities/notification';
+import { NotificationsRepository } from '../../../../application/repositories/notifications-repository';
+import { PrismaNotificationMapper } from '../mappers/prisma-notification-mappers';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class PrismaNotificationsReopository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
   async create(notification: Notification): Promise<void> {
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
     await this.prismaService.notification.create({
-      data: {
-        id: notification.id,
-        category: notification.category,
-        content: notification.content.value,
-        recipientId: notification.recipientId,
-      }
-    })
+      data: raw,
+    });
   }
 }
--- notifications-service/src/infra/http/controllers/notifications.controller.ts
+++ notifications-service/src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SendNotification } from 'src/application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
@@ -10,11 +11,13 @@ export class NotificationsController {
   async create(@Body() body: CreateNotificationBody) {
     const { recipientId, content, category } = body;
 
-    const { notification} = await this.sendNotification.execute({
+    const { notification } = await this.sendNotification.execute({
       recipientId,
       content,
       category,
     });
-    return { notification }
+    return {
+      notification: NotificationViewModel.toHTTP(notification)
+    };
   }
 }
